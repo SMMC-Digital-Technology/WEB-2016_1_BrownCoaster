a3509ebe23ddf7517ff9b1652eab7a063e9e2c44
SMMC-Web-Design <alan.burt@mackillop.act.edu.au>
2016-06-02 09:43:32 +1000

    Javascript v.1.1
    Added a function,
    Added a revised income calculation;
    Neatened code.

--- JavaScriptAss.js
+++ JavaScriptAss.js
@@ -1,16 +1,21 @@
 var Income = 0;
-var taxPaid = 0;
 
+function calculateTax(Income) {
+    var taxPaid = 0;
+    if (Income < 16000) {
+        taxPaid = 0;
+    } else if (Income < 52501) {
+        taxPaid = (Income - 16000) * 0.1;
+    } else if (Income < 130001) {
+        taxPaid = ((Income - 52500) * 0.16) + 3650;
+    } else if (Income < 184500) {
+        taxPaid = ((Income - 113000) * 0.24) + 13330;
+    } else {
+        taxPaid = ((Income - 184500) * 0.36) + 30490;
+    }
+    return taxPaid;
+}
 
-
-if (Income < 16000) {
-    taxPaid = 0;
-} else if (Income < 52501) {
-    taxPaid = (Income - 16000) * 0.1;
-} else if (Income < 13001) {
-    taxPaid = (Income - 52500) * 0.16;
-} else if (Income < 184500) {
-    taxPaid = (Income - 113000) * 0.24;
-} else {
-    taxPaid = (Income - 184500) * 0.36;
+function calculate() {
+    
 }
